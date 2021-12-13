e23bd529bd1bba22f7ec5013e64163408e9210c6
gil-unx <57790408+gil-unx@users.noreply.github.com>
2021-12-13 22:47:59 +0800

    Add files via upload

---
+++ pres/PresUnpack.cs
@@ -0,0 +1,234 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using GIL.FUNCTION;
+using System.IO.Compression;
+
+namespace GEBCS
+{
+    public class PresUnpack 
+    {
+        private Pres pres = new Pres();
+        private BR reader;
+        private Dictionary<long,int> grup = new Dictionary<long, int>();
+        private string outFolder;
+        private Dictionary<string, int> duplicateCek = new Dictionary<string, int>();
+        private string duplicate = "";
+        private FileStream fileStream;
+        public PresUnpack(string fileName)
+        {
+            fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);   
+            outFolder = Path.GetDirectoryName(fileName)+"\\" + Path.GetFileNameWithoutExtension(fileName) + "\\";
+            reader = new BR(fileStream);
+            pres.Filename = fileName;
+            pres.Magic = reader.ReadUInt32();
+            pres.GrupOffset = reader.ReadInt32();
+            pres.GrupCount = reader.ReadInt32();
+            pres.CeckSum = reader.ReadUInt32();
+            pres.TotalFile = 0;
+            pres.Grups = new List<int>();
+            for(int i = 0; i<8; i++)
+            {
+                int off = reader.ReadInt32();
+                int count = reader.ReadInt32();
+                pres.Grups.Add(off);
+                pres.Grups.Add(count);
+                if(off>0)
+                {
+                    grup.Add((long)off,count);
+                    pres.TotalFile+=count;
+                }
+                               
+            }                  
+            GenerateFileInfo();
+        }
+        private void GenerateFileInfo()
+        {
+            pres.Files = new List<Record>();
+            MemoryStream memory = new MemoryStream(reader.ReadBytes(pres.TotalFile*32));
+            pres.TocSize = (int)reader.BaseStream.Position;
+            BinaryReader toc = new BinaryReader(memory);
+            for(int i = 0; i < grup.Count; i++)
+            {
+                int count = grup[toc.BaseStream.Position+0x50];
+                for(int j = 0; j < count;j++)
+                {
+                    Record file = new Record();
+                    file.Offset = toc.ReadInt32();
+                    file.Size = toc.ReadInt32();
+                    file.OffsetName = toc.ReadInt32();
+                    file.ChunkName = toc.ReadInt32();
+                    toc.ReadBytes(16);
+                    file.UpdatePointer = new List<bool>(){
+                        Convert.ToBoolean(file.Offset),
+                        Convert.ToBoolean(file.Size),
+                        Convert.ToBoolean(file.OffsetName),
+                        Convert.ToBoolean(file.ChunkName),                       
+                    };
+                    if(file.ChunkName>0)
+                    {
+                        reader.BaseStream.Seek((long)file.OffsetName,SeekOrigin.Begin);
+                        file.ElementName = new List<string>();
+                        for(int k = 0; k <file.ChunkName; k++)
+                        {
+                            int offChunk = reader.ReadInt32();
+                            
+                            file.ElementName.Add(reader.GetUtf8((long)offChunk));
+                        }
+                        string dupName = String.Join("",file.ElementName).ToUpper();
+                        try
+                        {
+                            duplicate = String.Format("_{0,0:d4}",duplicateCek[dupName]);
+                            duplicateCek[dupName]++;                            
+                        }
+                        catch(Exception)
+                        {
+                            duplicateCek.Add(dupName,1);
+                        }
+                    }
+                    file.RealSize = file.Size;
+                    if(file.ChunkName == 0)
+                    {
+                        file.FileName = "dummy";
+                    }
+                    if(file.ChunkName == 1)
+                    {
+                        file.FileName = file.ElementName[0];
+                        file.RealSize = file.OffsetName-file.Offset;
+                    }
+                    if(file.ChunkName > 1)
+                    {
+                    	for(int k = 2; k < file.ChunkName;k++)
+                    	{
+                    		file.FileName += file.ElementName[k]+"\\";
+                    	}
+                    	file.FileName+=(file.ElementName[0]+duplicate+"."+file.ElementName[1]);
+                    }                   
+                    duplicate = "";
+                    int cek =  file.Offset+ file.RealSize+(((16 - (file.RealSize % 16) | 16))-16);
+                    if(cek == file.OffsetName)
+                    {
+                        file.Location = "Local";
+                    }
+                    else
+                    {
+                        if(file.ChunkName == 1)
+                        {
+                            file.Location = "Local";
+                        }
+                        else
+                        {
+                            if ((file.UpdatePointer[0] == true) && (file.UpdatePointer[1] == false) && (file.UpdatePointer[2] == true) && (file.UpdatePointer[3] == true))
+                            {
+                                file.RealSize = file.OffsetName - file.Offset;
+                                file.Location = "Local";
+                                file.ShiftOffset = 0;
+                            }
+                            else
+                            {
+                                file.Location = "Package";
+                                file.ShiftOffset = 15;
+                            }                          
+                        }
+                    }
+                    if(file.FileName == "dummy")
+                    {
+                        file.Location = "dummy";
+                    }
+                    pres.Files.Add(file);
+                }               
+            }                                                 
+        }
+        public void Unpack(ref BR package,ref ResNames resNames,ref Tr2Names tr2Names)
+        {
+            for(int i = 0; i < pres.TotalFile; i++)
+            {
+                Record file = pres.Files[i];
+                if ((file.FileName != "dummy")&& (file.Location != "dummy"))
+                {                  
+                    Directory.CreateDirectory(Path.GetDirectoryName(outFolder + file.FileName));
+                    Console.WriteLine("Extract: " + outFolder + file.FileName);
+                    int offset = file.Offset << file.ShiftOffset;
+                    int size = file.RealSize;
+                    byte[] comBuffer;
+                    switch (file.Location)
+                    {
+                        case "Local":
+                            comBuffer = reader.GetBytes((long)offset, size);                           
+                            break;
+                        case "Package":
+                            comBuffer = package.GetBytes((long)offset, file.Size);
+                            int padding = 0x8000 - (file.Size % 0x8000);
+                            if (padding == 0x8000) padding = 0;
+                            pres.Files[i].MaxSize = file.Size + padding;
+                            break;
+                        default:
+                            comBuffer = new byte[0];
+                            
+                            break;
+                    }
+                    if(comBuffer.Length > 0)
+                    {
+                        if ((comBuffer[0] == 0x1f) && (comBuffer[1] == 0x8b) && (comBuffer[2] == 0x08) && (comBuffer[4] == 0x00))
+                        {
+                            file.Compression = true;
+                            byte[] decBuffer = Decompress(comBuffer);
+                            File.WriteAllBytes(outFolder + file.FileName, decBuffer);
+                        }
+                        else
+                        {
+                            File.WriteAllBytes(outFolder + file.FileName, comBuffer);
+
+                        }                                               
+                    }
+                    else
+                    {
+                        File.WriteAllBytes(outFolder + file.FileName, comBuffer);
+                    }                   
+                }
+                if(Path.GetExtension(file.FileName) == ".res")
+                {
+                    resNames.Names.Add(outFolder + file.FileName);                                                        
+                    PresUnpack pres = new PresUnpack((outFolder + file.FileName));
+                    pres.Unpack(ref package, ref resNames , ref tr2Names);                  
+                }
+                if (Path.GetExtension(file.FileName) == ".tr2")
+                {
+                    tr2Names.Names.Add(outFolder + file.FileName);
+                    new Tr2Decoder(outFolder + file.FileName);
+                }
+            }
+            CreateXml();
+        }      
+        private void CreateXml()
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Pres));
+            TextWriter writer = new StreamWriter(Path.ChangeExtension(pres.Filename,"xml"));
+            xmlSerializer.Serialize(writer, pres);
+            writer.Close();
+        }
+        private byte[] Decompress(byte[] gzip)
+        {
+            using (GZipStream stream = new GZipStream(new MemoryStream(gzip),CompressionMode.Decompress))
+            {
+                const int size = 4096;
+                byte[] buffer = new byte[size];
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    int count = 0;
+                    do
+                    {
+                        count = stream.Read(buffer, 0, size);
+                        if (count > 0)
+                        {
+                            memory.Write(buffer, 0, count);
+                        }
+                    }
+                    while (count > 0);
+                    return memory.ToArray();
+                }
+            }
+        }
+    }
+}
---
+++ tr2/Tr2Encoder.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using GIL.FUNCTION;
+using System.Text;
+
+namespace GEBCS
+{
+    class Tr2Encoder
+    {
+        private Tr2 tr2 = new Tr2();
+        private BW writer;
+        public Tr2Encoder(string fileName)
+        {
+            Console.WriteLine("Encoding: "+fileName);
+            XmlSerializer tr2Serial = new XmlSerializer(typeof(Tr2));
+            Stream xmlReader = new FileStream(Path.ChangeExtension(fileName, "xml"), FileMode.Open, FileAccess.Read);
+            try
+            {
+                tr2 = (Tr2)tr2Serial.Deserialize(xmlReader);
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("");
+                return;
+            }
+           
+            FileStream fileStream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            
+            writer = new BW(fileStream);
+            writer.Write(Encoding.UTF8.GetBytes(tr2.Ext));
+            writer.Write(tr2.Unk);
+            writer.Write(Encoding.UTF8.GetBytes(tr2.InternalName));
+            writer.WritePadding(0x30, 8);
+            writer.Write(tr2.PointerOffset);
+            writer.Write(tr2.Count);
+            MemoryStream ms = new MemoryStream();
+            BW mw = new BW(ms);
+            int newOffset = tr2.PtStart;
+          
+          
+            foreach (Tr2Info tr2Info in tr2.Record)
+            {
+                byte[] buffer;
+                switch (tr2Info.EncodingType)
+                {
+                    case "UTF-8":
+                        if (tr2Info.PointerType == 0x69626F79)
+                        {
+                            buffer = Utf8Yobi(tr2Info);
+
+
+                        }
+                        else
+                        {
+                            buffer = Utf8(tr2Info);
+                        }
+                        break;
+                    case "ASCII":
+
+                        if (tr2Info.PointerType == 0x69626F79)
+                        {
+                            buffer = Utf8Yobi(tr2Info);
+                        }
+                        else
+                        {
+                            buffer = Utf8(tr2Info);
+                        }
+                        break;
+
+
+
+                    default:
+                       
+                        buffer = Convert.FromBase64String(tr2Info.ArrFile);
+                        break;
+                       
+
+                }
+                int newSize = buffer.Length;
+                int padding = 16 - (newSize % 16);
+                if (padding == 16) padding = 0;
+                newSize += padding;
+                writer.Write(tr2Info.Index);
+                writer.Write(newOffset);
+                writer.Write(tr2Info.PointerSize);
+                writer.Write(newSize);
+                writer.Write(newSize);
+                mw.Write(buffer);
+                mw.WritePadding(16, 0);
+                newOffset = (int)mw.BaseStream.Position+tr2.PtStart;
+               
+
+
+
+
+            }
+            if(tr2.UnkArr!= null) writer.Write(Convert.FromBase64String(tr2.UnkArr));
+            writer.Write(ms.ToArray());
+            fileStream.Flush();
+            fileStream.Close();
+             
+
+
+
+
+        }
+        private byte[] Utf8(Tr2Info tr2Info)
+        {
+            List<string> newStrings =  LoadTxt(tr2.FileName.Replace(".tr2", "\\") + tr2Info.Name + ".txt",tr2Info.Count);
+            MemoryStream ms = new MemoryStream();
+            BW mw = new BW(ms);
+            mw.Write(Encoding.UTF8.GetBytes(tr2Info.Name));
+            mw.BaseStream.Seek(0x30, SeekOrigin.Begin);
+            mw.Write(tr2Info.Dir);
+            mw.Write(0);
+            mw.Write(tr2Info.PointerType);
+            mw.Write(tr2Info.Unk1);
+            mw.Write(Encoding.UTF8.GetBytes(tr2Info.EncodingType));
+            mw.BaseStream.Seek(0x74, SeekOrigin.Begin);
+            mw.Write(tr2Info.Unk2);
+            mw.Write(tr2Info.Unk3);
+            mw.Write(tr2Info.Count);
+            int newOff = (tr2Info.Count * 4) + 0x88;
+            
+            for (int i = 0; i < tr2Info.Count; i++)
+            {
+                mw.Write(newOff);
+
+                if (tr2.InternalName == "anagura_progress[enus]") newStrings[i]=newStrings[i].Replace("\n", "<br>");
+                newOff += (int)Encoding.UTF8.GetBytes(newStrings[i]).Length;
+                newOff++;
+            }
+            mw.Write(newOff);
+            mw.BaseStream.Seek((long)(tr2Info.Count * 4) + 0x88,SeekOrigin.Begin);
+            for (int i = 0; i < tr2Info.Count; i++)
+            {
+                mw.Write(Encoding.UTF8.GetBytes(newStrings[i]));
+                mw.Write((byte)0);
+            }
+            mw.WritePadding(16, 0);
+            return ms.ToArray();
+        }
+        private byte[] Utf8Yobi(Tr2Info tr2Info)
+        {
+            List<string> newStrings = LoadTxt(tr2.FileName.Replace(".tr2", "\\") + tr2Info.Name + ".txt", tr2Info.Count);
+            MemoryStream ms = new MemoryStream();
+            BW mw = new BW(ms);
+            mw.Write(Encoding.UTF8.GetBytes(tr2Info.Name));
+            mw.BaseStream.Seek(0x30, SeekOrigin.Begin);
+            mw.Write(tr2Info.Dir);
+            mw.Write(0);
+            mw.Write(tr2Info.PointerType);
+            mw.Write(tr2Info.Unk1);
+            mw.Write(Encoding.UTF8.GetBytes(tr2Info.EncodingType));
+            mw.BaseStream.Seek(0x74, SeekOrigin.Begin);
+            mw.Write(tr2Info.Unk2);
+            mw.Write(tr2Info.Unk3);
+            mw.Write(tr2Info.Count);
+            int newOff = (tr2Info.Count * 12) + 0x80;
+            for (int i = 0; i < tr2Info.Count; i++)
+             {
+                mw.Write(tr2Info.IndexString[i]);
+                mw.Write(newOff);
+                int newLen = (int)Encoding.UTF8.GetBytes(newStrings[i]).Length;
+                mw.Write(newLen);
+                newOff += newLen;
+                newOff++;
+            }
+            mw.BaseStream.Seek((long)(tr2Info.Count * 12) + 0x80, SeekOrigin.Begin);
+            for (int i = 0; i < tr2Info.Count; i++)
+            {
+                mw.Write(Encoding.UTF8.GetBytes(newStrings[i]));
+                mw.Write((byte)0);
+            }
+            mw.WritePadding(16, 0);
+            return ms.ToArray();
+        }
+        private List<string> LoadTxt(string txtName, int count)
+        {
+            List<string> listString = new List<string>();
+            FileStream fileStream = new FileStream(txtName, FileMode.Open, FileAccess.Read);
+            StreamReader txtReader = new StreamReader(fileStream);
+            int txtIndex;
+            for (int i = 0; i < count; i++)
+            {
+                try
+                {
+                    txtIndex = int.Parse(txtReader.ReadLine().Substring(1, 4));
+                    if (txtIndex != i)
+                    {
+                        Console.WriteLine("Mismatch txt index");
+                        Environment.Exit(0);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Broken txt index");
+                    Environment.Exit(0);
+                }
+                string text = "";
+                while (true)
+                {
+                    string line = txtReader.ReadLine();
+
+                    if (line.Length > 10)
+                    {
+                        if (line.Substring(0, 10) == "----------")
+                        {
+                            text = text.Substring(0, text.Length - 1);
+
+                            break;
+                        }
+                    }
+                    text += line + "\n";
+                }
+
+                listString.Add(text);
+            }
+            fileStream.Close();
+            return listString;
+        }   
+    }
+
+
+}
